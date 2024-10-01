2fb255dc9fbd471b6b74f5471cffaa17b367b1c6
KaranSureshNair <karansuresh.cs23@bmsce.ac.in>
2024-10-01 07:12:34 +0530

    Add files via upload
    This is the source code and output for all the extra programs done on the 23rd of September

---
+++ first.java
@@ -0,0 +1,10 @@
+
+class first{
+public static void main(String s[])
+{
+System.out.print("Hello");
+add();
+}
+static void add()
+{System.out.println(" Good Morning");}
+ }
---
+++ second.java
@@ -0,0 +1,12 @@
+
+class second{
+    public static void main(String args[]) { 
+    int num; 
+    num=100;
+    System.out.print("This is num:"+num );
+    num=num*2;
+    System.out.print("The value of num * 2 is ");
+    System.out.println(num);
+    }
+    }
+    
